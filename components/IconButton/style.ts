051af24cb1813a69f7ea0358d24e6d85a2fcba1e
phongnhat19 <phongnhat19@gmail.com>
2020-11-26 10:32:02 +0700

    add IconButton components

---
+++ components/IconButton/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    badgeContainer: {
+        position: 'absolute',
+        top: -12,
+        right: -12,
+        backgroundColor: 'red',
+        padding: 5,
+        width: 25,
+        height: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 100
+    },
+    badgeText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        fontSize: 12
+    }
+})
