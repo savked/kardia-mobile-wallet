7d23e2229b4f089a36d01439146f76da143ff4a5
phongnhat19 <phongnhat19@gmail.com>
2020-12-01 11:28:12 +0700

    implement create wallet with private key

--- NoWalletStack.tsx
+++ NoWalletStack.tsx
@@ -4,6 +4,7 @@ import ImportWallet from './screens/ImportWallet';
 import CreateWallet from './screens/CreateWallet';
 import Welcome from './screens/Welcome';
 import ImportPrivateKey from './screens/ImportPrivateKey';
+import CreateWithPrivateKey from './screens/CreateWithPrivateKey';
 
 const NoWalletStack = createStackNavigator()
 
@@ -13,7 +14,8 @@ const NoWalletStackScreen = () => {
             <NoWalletStack.Screen name="Welcome" component={Welcome} />
             <NoWalletStack.Screen name="ImportWallet" component={ImportWallet} />
             <NoWalletStack.Screen name="ImportPrivateKey" component={ImportPrivateKey} />
-            <NoWalletStack.Screen name="CreateWallet" component={CreateWallet} options={{ title: 'Create new wallet' }} />
+            <NoWalletStack.Screen name="CreateWallet" component={CreateWallet} />
+            <NoWalletStack.Screen name="CreateWithPrivateKey" component={CreateWithPrivateKey} />
         </NoWalletStack.Navigator>
     );
 }
--- screens/CreateWallet/index.tsx
+++ screens/CreateWallet/index.tsx
@@ -1,10 +1,19 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { View, Text } from 'react-native'
+import Button from '../../components/Button'
+import { styles } from './style'
 
 const CreateWallet = () => {
+    const navigation = useNavigation()
     return (
-        <View>
-            <Text>Create Wallet</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Create new wallet</Text>
+            <View style={styles.optionContainer}>
+                <Button title="Create with Private key" type="primary" onPress={() => navigation.navigate('CreateWithPrivateKey')} />
+                <Button title="Create with Keystore file" type="primary" onPress={() => {}} />
+                <Button title="Create with mnemonic phrase" type="primary" onPress={() => {}} />
+            </View>
         </View>
     )
 }
