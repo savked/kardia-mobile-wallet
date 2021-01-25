52b4cabbdb5dc760843aacd90cee4c895fb5a41c
phongnhat19 <phongnhat19@gmail.com>
2021-01-25 17:31:09 +0700

    update config for production

--- services/config.ts
+++ services/config.ts
@@ -1,6 +1,6 @@
-export const ENDPOINT = 'https://backend-dev.kardiachain.io/api/v1/';
-// export const ENDPOINT = 'https://backend-1.kardiachain.io/api/v1/';
-export const ENDPOINT_GATEWAY = 'https://gateway-dev.kardiachain.io/api/v1/';
-// export const ENDPOINT_GATEWAY = 'https://backend-1.kardiachain.io/api/v1/';
-export const RPC_ENDPOINT = 'https://dev-1.kardiachain.io';
-// export const RPC_ENDPOINT = 'https://rpc.kardiachain.io';
+// export const ENDPOINT = 'https://backend-dev.kardiachain.io/api/v1/';
+export const ENDPOINT = 'https://backend-1.kardiachain.io/api/v1/';
+// export const ENDPOINT_GATEWAY = 'https://gateway-dev.kardiachain.io/api/v1/';
+export const ENDPOINT_GATEWAY = 'https://backend-1.kardiachain.io/api/v1/';
+// export const RPC_ENDPOINT = 'https://dev-1.kardiachain.io';
+export const RPC_ENDPOINT = 'https://rpc.kardiachain.io';
