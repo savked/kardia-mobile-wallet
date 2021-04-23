d2fe377b65851ce5efa50e73738d7d98c4ffe05d
phongnhat19 <phongnhat19@gmail.com>
2021-04-23 16:54:36 +0700

    update CustomText component

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -2,8 +2,31 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const CustomText = ({children, ...rest}: any) => {
+  // console.log(rest.style)
+  let finalStyle: any = {
+    fontFamily: 'WorkSans-Regular'
+  }
+
+  if (rest.style) {
+    if (Array.isArray(rest.style)) {
+      finalStyle = [
+        {
+          fontFamily: 'WorkSans-Regular'
+        },
+        ...rest.style,
+      ]
+    } else {
+      finalStyle = [
+        {
+          fontFamily: 'WorkSans-Regular'
+        },
+        rest.style
+      ]
+    }
+  }
+
   return (
-    <Text {...rest} style={[rest.style, {fontFamily: 'Work Sans'}]}>{children}</Text>
+    <Text {...rest} style={finalStyle}>{children}</Text>
   );
 };
 
