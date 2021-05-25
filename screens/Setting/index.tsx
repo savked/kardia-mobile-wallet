453ad531527b5da92f2b86ffa8ec0219b40ae7cc
phongnhat19 <phongnhat19@gmail.com>
2021-05-25 14:53:39 +0700

    update version

--- screens/Setting/index.tsx
+++ screens/Setting/index.tsx
@@ -17,7 +17,7 @@ import { fontSizeAtom } from '../../atoms/fontSize';
 import { saveFontSize } from '../../utils/local';
 
 export const INFO_DATA = {
-  version: '2.2.0',
+  version: '2.2.1',
 };
 
 const SettingScreen = () => {
