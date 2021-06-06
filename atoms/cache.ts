d0d374053b22b4bd565141c1da177ddd0ccc183c
phongnhat19 <phongnhat19@gmail.com>
2021-06-06 11:40:35 +0700

    add cache atom

---
+++ atoms/cache.ts
@@ -0,0 +1,24 @@
+import {atom, selectorFamily} from 'recoil';
+
+export const DEFAULT_CACHE: Record<string, any> = {
+  slippageTolerance: '0.1',
+  txDeadline: '2'
+};
+
+export const cacheAtom = atom({
+  key: 'cacheAtom', // unique ID (with respect to other atoms/selectors)
+  default: DEFAULT_CACHE,
+});
+
+export const cacheSelector = selectorFamily({
+  key: 'cacheSelector',
+  get: (key: string) => ({get}) => {
+    const cache = get(cacheAtom)
+    return cache[key]
+  },
+  set: (key) => ({set}, value) => {
+    set(cacheAtom, (prevState) => {
+      return {...prevState, [key]: value}
+    })
+  },
+})
