fb3f8fe7a5ad0c3de794f59394917e097b72afed
phongnhat19 <phongnhat19@gmail.com>
2021-06-09 13:56:26 +0700

    remove unnecessary selector

--- atoms/wallets.ts
+++ atoms/wallets.ts
@@ -9,17 +9,3 @@ export const selectedWalletAtom = atom({
   key: 'selectedWalletAtom',
   default: 0,
 });
-
-export const getSelectedWallet = selectorFamily({
-  key: 'selectedWalletSelector',
-  get: () => ({get}) => {
-    const wallets = get(walletsAtom)
-    const selectedWallet = get(selectedWalletAtom)
-
-    if (selectedWallet > wallets.length - 1) {
-      return wallets[wallets.length - 1]
-    }
-
-    return wallets[selectedWallet]
-  },
-});
