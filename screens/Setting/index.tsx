14e1b4d8508af2d9560ef20212176da798d9f9c8
phongnhat19 <phongnhat19@gmail.com>
2021-08-02 13:45:33 +0700

    update app version

--- screens/Setting/index.tsx
+++ screens/Setting/index.tsx
@@ -19,7 +19,7 @@ import ReferralCodeModal from '../common/ReferralCodeModal';
 import { statusBarColorAtom } from '../../atoms/statusBar';
 
 export const INFO_DATA = {
-  version: '2.3.16',
+  version: '2.3.17',
 };
 
 const SettingScreen = () => {
